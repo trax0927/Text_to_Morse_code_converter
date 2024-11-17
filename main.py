007c94aaf1d4897769341ed0a292f91f05b94ecf
Trax <adenmosunpaulo@gmail.com>
2024-11-17 20:51:24 +0100

    sucessfully converted first trail

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-# TODO 1: prompt the user for his or her text and return the word
+# TODO 1: function to convert the text to morse:
+def to_morse_code(text: str) -> str:
+    """
+    converts text to morse code
+    :param text: the text user wants to convert.
+    :return: the morse code of the text
+    """
+    pass
+
 
 # TODO 2: get morse code for alphabets and numbers in a dictionary
+morse_code = {
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',
+    'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---',
+    'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',
+    'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+    'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--',
+    'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--',
+    '/': '-..-.',  '(': '-.--.',  ')': '-.--.-', '&': '.-...', ':': '---...',
+    ';': '-.-.-.', '=': '-...-',  '+': '.-.-.',  '-': '-....-', '_': '..--.-',
+    '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'  # Space as separator
+}
 
-# TODO 3: check the corresponding text morse code and return it
+# TODO 3: prompt the user for his or her text and return the word
+Texts = input('Enter a Text you want to convert to morse code: ').upper()
+Texts = list(Texts)
+
+for Text in Texts:
+    print(morse_code[Text])
