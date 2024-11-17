ae1e97e369425a025a2af21183d9f66fb8e18f66
Trax <adenmosunpaulo@gmail.com>
2024-11-17 16:14:08 +0100

    inntial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# TODO 1: prompt the user for his or her text and return the word
+
+# TODO 2: get morse code for alphabets and numbers in a dictionary
+
+# TODO 3: check the corresponding text morse code and return it
